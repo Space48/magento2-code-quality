100349585eeece5ff9a08811fcbe9263d8028817
Tomas Gerulaitis <tomas.gerulaitis@space48.com>
2021-04-16 16:26:32 +0100

    S48-638: Configure stylelint task

--- script/install.sh
+++ script/install.sh
@@ -2,9 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 
-npm install eslint --save-dev
+npm install eslint stylelint --save-dev
 
 [ -f ./ruleset.xml ] || cp $DIR/ruleset.xml ./
 [ -f ./phpmd.xml ] || cp $DIR/phpmd.xml ./
 [ -f ./.eslint ] || cp $DIR/.eslintrc ./
+[ -f ./.stylelintrc ] || cp $DIR/.stylelintrc ./
 [ -f ./grumphp.yml ] || cp $DIR/grumphp.yml ./
